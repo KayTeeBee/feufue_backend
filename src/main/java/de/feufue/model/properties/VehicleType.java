08ec69932c163c33098ebe7e2c2fe627f2a432c6
Kim 'KTB' Lausten <k-o.lausten.1094@outlook.com>
2026-04-25 12:37:32 +0200

    simplified vehicle types

--- src/main/java/de/feufue/model/properties/VehicleType.java
+++ src/main/java/de/feufue/model/properties/VehicleType.java
@@ -1,66 +1,59 @@
 package de.feufue.model.properties;
 
+import java.util.List;
+
 public enum VehicleType {
     
-    WACHE("Wache", "00"),
-    LANDESLEITUNG("Landesleitung", "01"),
-    AMTSLEITUNG("Amtsleitung", "02"),
-    GEMEINDELEITUNG("Gemeindeleitung", "03")
-    ORTSLEITUNG("Ortsleitung", "04"),
-    EINSATZABSCHNITTSLEITUNG("Einsatzabschnittsleitung", "05"),
-    EINHEITSFUEHRUNG("Einheitsführung", "06"),
-    FERNMELDEBETRIEBSDIENST("Fernmeldebetriebsdienst", "07"),
-    FACHBERATUNG("Fachberatung", "08"),
-    FF("Führungsfahrzeug", "10"),
-    ELW_FUEHRUNG("ELW Führung", "11"),
-    ELW_STABSFUEHRUNG("ELW Stabsführung", "12"),
-    F_TECH("Fahrzeug zur technischen Unterstüzung", "13"),
-    F_ORGA("Fahrzeug zur organisatorischen Unterstüzung", "14"),
-    KR("Kraftrad", "15"),
-    LB("Luftbeobachter", "16"),
-    PKW("PKW", "17"),
-    MTW("MTW", "18"),
-    TLF_10_25("TLF 10/25", "20"),
-    TLF_10_25_SONDERLOESCHMITTEL("TLF 10/25 mit Sonderlöschmittel", "21"),
-    TLF_20_35("TLF 20/35", "22"),
-    TLF_20_35_SONDERLOESCHMITTEL("TLF 20/35 mit Sonderlöschmittel", "23"),
-    TLF_20_50("TLF 20/50", "24"),
-    TLF_20_50_SONDERLOESCHMITTEL("TLF 20/50 mit Sonderlöschmittel", "25"),
-    TLF_20_U50("TLF 20/Ü50", "26"),
-    FZ_SONDERLOESCHMITTEL("Fahrzeug mit Sonderlöschmittel", "27"),
-    SONSTIGE_TLF("Sonstige TLF", "29"),
-    DLK_K18_12("DLK K18/12", "30"),
-    DLK_18_12("DLK 18/12", "31"),
-    DLK_23_12("DLK 23/12", "32"),
-    HAB_K18_12("HAB K18/12", "34"),
-    HAB_18_12("HAB 18/12", "35"),
-    HAB_23_12("HAB 23/12", "36"),
-    FWK("FWK", "37"),
-    LF_10("LF 10", "40"),
-    LF_10_05("LF 10/05", "41"),
-    LGF_10("LGF 10", "42"),
-    LGF_10_05("LGF 10/05", "43"),
-    LGF_10_05_PFPN("LGF 10/05 mit PFPN", "44"),
-    LGF_10_05_TH("LGF 10/05 TH", "45"),
-    LGF_20_10("LGF 20/10", "46"),
-    LGF_20_10_PFPN("LGF 20/10 mit PFPN", "47"),
-    LGF_20_10_TH("LGF 20/10 TH", "48"),
-    SONSTIGE_LF("Sonstige LF", "49");
+    PKW("PKW", "Personenkraftwagen", List.of("17")),
+    MTW("MTW", "Mannschaftstransportwagen", List.of("18")),
+    TLF_10("TLF 10", "Tanklöschfahrzeug 10", List.of("20", "21")),
+    TLF_20("TLF 20", "Tanklöschfahrzeug 20", List.of("22", "23", "24", "25", "26")),
+    DL("DL", "Drehleiter", List.of("30", "31", "32")),
+    HAB("HAB", "Hubarbeitsbühne", List.of("34", "35", "36")),
+    FWK("FwK", "Feuerwehrkran", List.of("38")),
+    LF_10("LF 10", "Löschfahrzeug 10", List.of("40", "41", "42", "43", "44", "45")),
+    LF_20("LF 20", "Löschfahrzeug 20", List.of("46", "47", "48")),
+    RW("RW", "Rüstungswagen", List.of("50", "51")),
+    GW("GW", "Gerätewagen", List.of("54", "55", "56", "57", "58")),
+    SW("SW", "Schlauchwagen", List.of("60", "61")),
+    WLF("WLF", "Wechselladerfahrzeug", List.of("62", "63", "64")),
+    LKW("LKW", "Lastkraftwagen", List.of("65", "66", "67", "68")),
+    ITW("ITW", "Itensivtransportwagen", List.of("80")),
+    NEF("NEF", "Notarzteinsatzfahrzeug", List.of("82")),
+    RTW("RTW", "Rettungswagen", List.of("83")),
+    KTW("KTW", "Krankentransportwagen", List.of("85"));
+
 
     private final String displayedName;
-    private final String number;
+    private final String vehicleName;
+    private final List<String> numbers;
 
-    private VehicleType(String displayedName, String number) {
+    private VehicleType(String displayedName, String vehicleName, List<String> numbers) {
         this.displayedName = displayedName;
-        this.number = number;
+        this.vehicleName = vehicleName;
+        this.numbers = numbers;
     }
 
     public String getDisplayedName() {
         return displayedName;
     }
 
-    public String getNumber() {
-        return number;
+    public String getVehicleName() {
+        return vehicleName;
+    }
+
+    public List<String> getNumbers() {
+        return numbers;
+    }
+
+    public VehicleType getVehicleTypeByVehicleName(String vehicleName) {
+        VehicleType[] vehicleTypes = values();
+        for (VehicleType vt : vehicleTypes) {
+            if (vt.getVehicleName().equals(vehicleName)) {
+                return vt;
+            }
+        }
+        throw new IllegalArgumentException("No VehicleType with vehicle name " + vehicleName + " found.");
     }
 
     public VehicleType getVehicleTypeByDisplayedName(String displayedName) {
@@ -76,7 +69,7 @@ public enum VehicleType {
     public VehicleType getVehicleTypeByNumber(String number) {
         VehicleType[] vehicleTypes = values();
         for (VehicleType vt : vehicleTypes) {
-            if (vt.getNumber().equals(number)) {
+            if (vt.getNumbers().contains(number)) {
                 return vt;
             }
         }
